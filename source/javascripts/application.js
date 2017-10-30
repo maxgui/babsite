d45ea2995b77c3548c5114676fb6d3ea33571c8b
Maxime Guillemain <maxguillemain@gmail.com>
2017-10-30 17:18:47 +0100

    changes on home

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,13 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+$("#pop").on("click", function() {
+   $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+   $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+
+
+$('#myCarousel').carousel({
+  interval: 100
+});
